53cf8d689a78f6216148334cb105ef19753c56a8
mayankjha2021 <mayankk.ug21.ee@nitp.ac.in>
2024-08-10 00:53:12 +0530

    first commit

---
+++ Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+        <ul>
+           <li>Home</li>
+           <li>about</li>
+           <li>contact</li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Header
